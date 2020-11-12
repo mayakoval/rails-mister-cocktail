1d74f7be4ba4eb83c5a1e952ac2c6f2250b3d33f
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2020-11-12 15:30:25 +0000

    Website functioning but ugly

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,32 @@
 class DosesController < ApplicationController
   def new
+  	@cocktail = Cocktail.find(params[:cocktail_id])
+  	@dose = Dose.new
+  	@ingredients = Ingredient.all
   end
 
   def create
+  	# get cocktail_id from url, won't be needed in dose_param
+  	@cocktail = Cocktail.find(params[:cocktail_id])
+  	@dose = Dose.new(dose_params)
+  	@dose.cocktail = @cocktail
+
+  	if @dose.save
+  		redirect_to cocktail_path(@cocktail)
+  	else
+  		render :new
+  	end
   end
 
   def destroy
+  	@dose = Dose.find(params[:id])
+  	@dose.destroy
+  	redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+  	params.require(:dose).permit(:description, :ingredient_id)
   end
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,5 +3,6 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
 
   validates :description, presence: true
+  # belongs_to makes the link with ingredient_id and cocktail_id
   validates :cocktail, uniqueness: { scope: :ingredient }
 end
